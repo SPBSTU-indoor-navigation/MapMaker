681fc2023bcb6d3b2f2e008cf195bad81bc911ca
Andrei <soprachev@mail.ru>
2022-05-29 08:03:26 +0300

    upd

--- Assets/Map/Scripts/LRCirclePattern.cs
+++ Assets/Map/Scripts/LRCirclePattern.cs
@@ -11,6 +11,12 @@ public class LRCirclePattern : MonoBehaviour
     public float radius = 5, start, end = 360;
     public int count = 20;
 
+    [Header("Ellipse")]
+    public float b = -1;
+
+    [Space]
+    public Vector2 offset;
+
     void Start()
     {
         lr = GetComponent<LineRenderer>();
@@ -25,9 +31,41 @@ public class LRCirclePattern : MonoBehaviour
         float step = Mathf.Deg2Rad * (end - start) / count;
         float st = Mathf.Deg2Rad * start;
 
-        for (int i = 0; i <= count; i++)
+        if (b < 0)
         {
-            l.Add(new Vector3(radius * Mathf.Sin(st + i * step), radius * Mathf.Cos(st + i * step), -5));
+            for (int i = 0; i <= count; i++)
+            {
+
+                float t = st + i * step;
+                l.Add(new Vector3(
+                    radius * Mathf.Sin(t) + (i < count / 2 ? offset.x / 2 : -offset.x / 2),
+                    radius * Mathf.Cos(t) + (i < count / 2 ? offset.y / 2 : -offset.y / 2),
+                    -5));
+            }
+        }
+        else
+        {
+
+            float e = Mathf.Sqrt(1 - (b / radius) * (b / radius));
+            for (int i = 0; i <= count; i++)
+            {
+                float t = st + i * step;
+
+                float r = Mathf.Sqrt(
+                    b * b / (1 - e * e * Mathf.Cos(t) * Mathf.Cos(t))
+                );
+                Vector3 pos = new Vector3(
+                    r * Mathf.Sin(t) + (i < count / 2 ? offset.x / 2 : -offset.x / 2),
+                    r * Mathf.Cos(t) + (i < count / 2 ? offset.y / 2 : -offset.y / 2),
+                    -5
+                );
+                l.Add(pos);
+            }
+        }
+
+        if (offset.sqrMagnitude > 0.1f)
+        {
+            l.Add(l[0]);
         }
 
         lr.positionCount = l.Count;
